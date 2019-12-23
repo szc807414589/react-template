ef250abf1993d1dc26ffa864bbf1de8dba9e6e4c
szc807414589 <807414589@qq.com>
2019-12-23 17:34:08 +0800

    改了半天的 路由切换动画

--- src/router/index_1.js
+++ src/router/index_1.js
@@ -3,11 +3,13 @@ import { Router, Switch, Route, Redirect, withRouter } from "react-router-dom"
 import HomePageLayout from '../layouts/HomePage'
 import history from "../history"
 import { TransitionGroup, CSSTransition } from "react-transition-group"
-import Login from '../pages/login'
-import Home from '../pages/home/index'
-import Benifi from '../pages/home/benefit'
-
 import './style.less'
+
+const Home = React.lazy(() => import('../pages/home/index'))
+const Benefit = React.lazy(() => import('../pages/home/benefit'))
+const Login = React.lazy(() => import('../pages/login'))
+const Policy = React.lazy(() => import('../pages/user/policy'))
+
 
 const Loading = () => {
 	return (
@@ -39,10 +41,7 @@ const HomePage = () => {
 const UserPage = () => {
 	return (
 		<Switch>
-			<Route
-				path={ '/userPage/policy' }
-				component={ React.lazy(() => import('../pages/user/policy')) } />
-
+			<Route path={ '/userPage/policy' } component={ React.lazy(() => import('../pages/user/policy')) } />
 		</Switch>
 	)
 }
@@ -67,44 +66,74 @@ const ACTION_MAP = {
 	PUSH: 'forward',
 	POP: 'back'
 }
-const TransRouter = withRouter(({ location, history }) => (
-	//<Suspense fallback={ <Loading /> }>
-	<TransitionGroup
-		className={ 'router-wrapper' }
-		childFactory={ child => React.cloneElement(
-			child,
-			{ classNames: ACTION_MAP[history.action] }
-		) }
-	>
-		<CSSTransition
-			timeout={ 500 }
-			key={ location.pathname }
-			// appear={ true }
+
+
+let oldPathName = null
+const TransRouter = withRouter(({ location, history }) => {
+	console.log(history)
+	const { pathname } = location
+
+
+	oldPathName = pathname
+	return (
+		<TransitionGroup
+			className={ 'router-wrapper' }
+			childFactory={ child => React.cloneElement(
+				child,
+				{ classNames: pathname === '/home' || pathname === '/home/user' ? `Tab-${ ACTION_MAP[history.action] }` : ACTION_MAP[history.action] }
+				//{ classNames: ACTION_MAP[history.action] }
+			) }
 		>
-			<Switch location={ location }>
-				<Route path={ '/login' }
-				       component={Login
-					       // React.lazy(() =>
-						   //     import('../pages/login')
-					       // )
-				       } />
-				<Route path={ '/home' }
-				       component={Home
-					       // React.lazy(() =>
-						   //     import('../pages/home/index')
-					       // )
-				       } />
-				<Route path={ '/homePage/benefit' }
-				       component={Benifi
-					       // React.lazy(() =>
-						   //     import('../pages/home/benefit')
-					       // )
-				       } />
-			</Switch>
-		</CSSTransition>
-	</TransitionGroup>
-	//</Suspense>
-))
+			<CSSTransition
+				timeout={ 10000 }
+				// timeout={ pathname === '/home' || pathname === '/home/user' ? 0 : 10000 }
+				// key={ pathname }
+				appear={ true }
+				mountOnEnter={true}
+				in={ pathname !== '/home' || pathname !== '/home/user' }
+				addEndListener={(node, done) => {
+					// debugger
+					console.log(node.children[0].className)
+				}}
+			>
+				<div>
+					<Suspense fallback={ <Loading /> }>
+						<Switch location={ location }>
+							<Route path={ '/login' } component={ Login } />
+							<Route path={ '/homePage/benefit' } component={ Benefit } />
+							<Route path={ '/userPage/policy' } component={ Policy } />
+							<Route path={ '/' } component={ TabRouter } />
+							{/*<Redirect path={ '/' } to={ '/home' } />*/ }
+						</Switch>
+					</Suspense>
+				</div>
+			</CSSTransition>
+		</TransitionGroup>
+	)
+})
+/*
+*
+*  1
+* old : exit exit-active
+* new : enter enter-active
+*  2
+* old : 移除
+* new : enter-done
+*
+*   tab页面需要单独处理.tab切换的时候不做动画处理
+*
+*
+*
+*
+* */
+const ROUTER = () => (
+	<Suspense fallback={ <Loading /> }>
+		<Switch>
+			<Route path={ '/home' } component={ TabRouter } />
+			<Route path={ '/' } component={ TransRouter } />
+		</Switch>
+	</Suspense>
+)
 
 const AppRouter1 = () => (
 	<Router history={ history }>
